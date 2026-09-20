e08be0f6c358a3ebaa517a5fab562b0c9202fdd6
aftab mogal <mogalaftab940@gmail.com>
2026-09-21 02:16:59 +0530

    Pin Python 3.12 for Render (fix Django 5.0/3.14 incompatibility)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,3 +115,18 @@ FRONTEND_URL = config("FRONTEND_URL", default="http://localhost:5173")
 
 # Subscription flat fees used for prize-pool math (INR)
 SUBSCRIPTION_FEES = {"monthly": 999, "yearly": 9999}
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {"handlers": ["console"], "level": "WARNING"},
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
